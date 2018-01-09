65eb896eff613fb804c72f4f209fc11d8cd2784f
JGo <244967114@qq.com>
2018-01-10 05:15:01 +0800

    1-10 add Baidu-Map SDK into project.

--- app/src/main/java/cn/nju/lee/walked/MainActivity.java
+++ app/src/main/java/cn/nju/lee/walked/MainActivity.java
@@ -3,16 +3,43 @@ package cn.nju.lee.walked;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.nju.lee.walked.view.widget.DemoActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.bmapview_demo)
+    MapView mMapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        DemoActivity.activityStart(this, "a");
+        ButterKnife.bind(this);
+//        DemoActivity.activityStart(this, "a");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
 }
