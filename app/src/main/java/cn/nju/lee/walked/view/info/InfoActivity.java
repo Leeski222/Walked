80d25c5c72214b852fa4edb0f7cf95fb074d74ef
JGo <244967114@qq.com>
2018-03-21 23:33:12 +0800

    3-21 complete the function of show html in rich editor.

---
+++ app/src/main/java/cn/nju/lee/walked/view/info/InfoActivity.java
@@ -0,0 +1,10 @@
+package cn.nju.lee.walked.view.info;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 果宝 on 2018/3/21.
+ */
+
+public class InfoActivity extends AppCompatActivity {
+}
